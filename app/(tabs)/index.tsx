d069e89c23c1a4c9ef2b3ec114a5c0349630d8fd
arirawr12 <3122ienxaza@gmail.com>
2026-03-02 17:46:52 -0600

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { router } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
 
 export default function HomeScreen() {
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>🔐 CONTROL-SEGURITY</Text>
       <Text style={styles.welcome}>Bienvenido al sistema</Text>
+
+      <TouchableOpacity 
+  style={styles.button}
+  onPress={() => router.push('/register')}
+>
+  <Text style={styles.buttonText}>Crear Cuenta</Text>
+</TouchableOpacity>
     </View>
-  );
+);
+    
+  
 }
 
 const styles = StyleSheet.create({
@@ -29,4 +41,16 @@ const styles = StyleSheet.create({
     color: '#FF0000',
     fontSize: 18,
   },
+  button: {
+  backgroundColor: '#FF0000',
+  padding: 12,
+  borderRadius: 8,
+  marginTop: 20,
+},
+
+buttonText: {
+  color: '#000',
+  fontWeight: 'bold',
+},
+
 });
