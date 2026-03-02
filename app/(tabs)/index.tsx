369d0826545b4c038470fb3c2094f3aae718f302
arirawr12 <3122ienxaza@gmail.com>
2026-03-02 17:27:53 -0600

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -59,22 +59,6 @@ useEffect(() => {
 
 const styles = StyleSheet.create({
   
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
   container: {
   flex: 1,
   justifyContent: 'center',
