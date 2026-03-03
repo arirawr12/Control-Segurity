2961410b781591411ff44d6f1aca48439134e1ef
arirawr12 <3122ienxaza@gmail.com>
2026-03-02 18:55:25 -0600

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,49 @@
-import { DarkTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 100, // espacio para header
+  },
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 40,
+    left: 20,
+  },
 
-export default function RootLayout() {
-  return (
-    <ThemeProvider value={DarkTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen 
-          name="modal" 
-          options={{ 
-            presentation: 'modal', 
-            title: 'Modal',
-            headerStyle: { backgroundColor: '#000' },
-            headerTintColor: '#FF0000'
-          }} 
-        />
-      </Stack>
-      <StatusBar style="light" />
-    </ThemeProvider>
-  );
-}
+  appLogo: {
+    width: 35,
+    height: 35,
+    marginRight: 10,
+  },
+
+  appName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  title: {
+    color: '#FF0000',
+    fontSize: 24,
+    fontWeight: '600',
+    marginBottom: 40,
+    textAlign: 'center',
+  },
+
+  button: {
+    backgroundColor: '#FF0000',
+    padding: 12,
+    borderRadius: 8,
+    marginTop: 20,
+  },
+
+  buttonText: {
+    color: '#000',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
