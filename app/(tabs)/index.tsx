e3439414831b5f3f4739ca7212d52270320212d5
arirawr12 <3122ienxaza@gmail.com>
2026-03-02 19:42:59 -0600

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
+
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { router } from 'expo-router';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
 
-      {/* HEADER: TITULO ARRIBA IZQUIERDA */}
+      {/* HEADER: LOGO + TITULO ARRIBA IZQUIERDA */}
       <View style={styles.header}>
+        <Image
+          source={require('@/assets/images/logo.jpeg')}
+          style={styles.logo}
+        />
         <Text style={styles.appName}>Control-Segurity</Text>
       </View>
 
@@ -31,13 +36,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: 80, // espacio para el header
+    paddingTop: 80,
   },
 
   header: {
+    flexDirection: 'row',
+    alignItems: 'center',
     position: 'absolute',
     top: 40,
     left: 20,
+  },
+
+  logo: {
+    width: 35,
+    height: 35,
+    borderRadius: 5,
+    marginRight: 10,
   },
 
   appName: {
@@ -66,4 +80,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  
 });
